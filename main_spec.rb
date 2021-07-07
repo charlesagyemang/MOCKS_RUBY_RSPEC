7158e102c5889a4c972771a45a18fec3098d032b
charlesagyemang <micnkru@gmail.com>
2021-07-07 01:19:44 +0000

    update

--- main_spec.rb
+++ main_spec.rb
@@ -5,7 +5,12 @@ describe PolyTank do
     describe "#fill_poly_tank" do
         it 'should have maximum water' do
             tank = PolyTank.new(40)
-            pump = Pump.new
+            # pump = Pump.new
+            # instead of actually calling pump which is the 
+            # dependency we are mocking the pum with a double and 
+            # returning a desired output for our test to pass
+
+            pump = double("Pump", dispense_water: 100)
             tank.fill_poly_tank(pump)
 
             expect(tank.water_level).to  eq(100)
